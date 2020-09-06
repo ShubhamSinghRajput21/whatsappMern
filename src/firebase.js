f6b4f72cc001dd1d2b52b1558e7eafdba916b598
ShubhamSingh <ss889469@gmail.com>
2020-09-06 20:45:14 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAC1iN7TpsqT9Aw2EELx-L4OfK74lP23Eg",
+  authDomain: "whatsapp-mern-5279a.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-5279a.firebaseio.com",
+  projectId: "whatsapp-mern-5279a",
+  storageBucket: "whatsapp-mern-5279a.appspot.com",
+  messagingSenderId: "637030387363",
+  appId: "1:637030387363:web:dd5e83c84fdc1fc033f916",
+};
