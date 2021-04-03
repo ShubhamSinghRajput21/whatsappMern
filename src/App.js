1993174a7c337baed8b9fed77a55549f619bdde0
ShubhamSingh <ss889469@gmail.com>
2021-04-03 11:39:14 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import Pusher from "pusher-js";
+import axios from "./axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    const pusher = new Pusher("8c9d9498b4e134bb6e32", {
+      cluster: "eu",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", function (data) {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
+  console.log(messages);
+
   return (
     <div className="app">
       <div className="app_body">
--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,65 @@
 import React from "react";
 import "./Chat.css";
-
+import { Avatar, IconButton } from "@material-ui/core";
+import {
+  SearchOutlined,
+  AttachFile,
+  MoreVert,
+  InsertEmoticon,
+} from "@material-ui/icons";
+import MicIcon from "@material-ui/icons/Mic";
 export default function Chat() {
   return (
     <div className="chat">
-      <h1>I am a Chat Component</h1>
+      <div className="chat_header">
+        <Avatar />
+        <div className="chat_headerInfo">
+          <h3>Roomname</h3>
+          <p>Last seen at ....</p>
+        </div>
+        <div className="chat_headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat_body">
+        <p className="chat_message">
+          <span className="chat_name">Soony</span>
+          This is a message
+          <span className="chat_timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat_message">
+          <span className="chat_name">Soony</span>
+          This is a message
+          <span className="chat_timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat_message chat_reciever">
+          <span className="chat_name">Soony</span>
+          This is a message
+          <span className="chat_timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat_message">
+          <span className="chat_name">Soony</span>
+          This is a message
+          <span className="chat_timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+      <div className="chat_footer">
+        <InsertEmoticon />
+        <form>
+          <input placeholder="Type a message" type="text" />
+          <button type="submit">Send a message</button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
   );
 }
